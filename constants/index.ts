ab05796cf20a25735cda994b7304b94736977e1b
unknown <dev-kadere>
2025-06-14 02:29:16 +0300

    initil

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    label: "Testimonials",
+    href: "#testimonials",
+  },
+  {
+    label: "FAQS",
+    href: "#faqs",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+];
---
+++ sections/Header.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className=" container !max-w-full">
+        <div className="flex justify-between h-20 items-center">
+          <div>
+            <Link href="/">
+              <span className="text-xl font-bold uppercase">
+                Nicholas Ndereba
+              </span>
+            </Link>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className=" size-11 border border-stone-400 rounded-full inline-flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <rect x="3" y="15" width="18" height="2" fill="currentColor" />
+                <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+              </svg>
+            </div>
+            <button
+              className="hidden md:inline-flex btn-red-orange text-white
+             h-11 px-6 rounded-lg border border-red-orange uppercase  cursor-pointer items-center "
+            >
+              Contact Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
